c29a71f3fc1ebea8fe48bcaf8a4ab2c462941b4d
paul-beckett <beckettp@gmail.com>
2025-12-02 20:35:52 +0000

    aoc2025 - 2025 day02 part1 and 2

--- challenge/aoc2025/day02/day02.go
+++ challenge/aoc2025/day02/day02.go
@@ -1,7 +1,6 @@
 package day02
 
 import (
-	"errors"
 	"strconv"
 	"strings"
 )
@@ -15,23 +14,66 @@ func NewDay02(data []string) *Day02 {
 }
 
 func (d *Day02) Part1() (int, error) {
-	sum := 0
-	ranges := strings.FieldsFunc(d.data[0], func(r rune) bool { return r == ',' })
-	for _, r := range ranges {
+	return d.sumInvalid(func(s string) bool {
+		return len(s)%2 == 0 && isRepeating(s, len(s)/2)
+	}), nil
+}
+
+func (d *Day02) Part2() (int, error) {
+	return d.sumInvalid(isInvalid), nil
+}
+
+type Range struct {
+	low  int
+	high int
+}
+
+func parseRanges(s string) []Range {
+	var ranges []Range
+	fields := strings.FieldsFunc(s, func(r rune) bool { return r == ',' })
+	for _, r := range fields {
 		f := strings.Split(r, "-")
 		low, _ := strconv.Atoi(f[0])
 		high, _ := strconv.Atoi(f[1])
-		for i := low; i <= high; i++ {
-			s := strconv.Itoa(i)
-			l := len(s)
-			if l%2 == 0 && s[:l/2] == s[l/2:] {
+		ranges = append(ranges, Range{
+			low:  low,
+			high: high,
+		})
+	}
+	return ranges
+}
+
+func isInvalid(s string) bool {
+	l := len(s)
+	for n := 1; n <= l/2; n++ {
+		if isRepeating(s, n) {
+			return true
+		}
+	}
+	return false
+}
+
+func isRepeating(s string, l int) bool {
+	if len(s)%l != 0 {
+		return false
+	}
+	for i := 0; i < len(s)-l; i += l {
+		if s[i:i+l] != s[i+l:i+l+l] {
+			return false
+		}
+	}
+	return true
+}
+
+func (d *Day02) sumInvalid(f func(string) bool) int {
+	sum := 0
+	ranges := parseRanges(d.data[0])
+	for _, r := range ranges {
+		for i := r.low; i <= r.high; i++ {
+			if f(strconv.Itoa(i)) {
 				sum += i
 			}
 		}
 	}
-	return sum, nil
-}
-
-func (d *Day02) Part2() (int, error) {
-	return 0, errors.ErrUnsupported
+	return sum
 }
