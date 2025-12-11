d4d7320507f23d22985383d43ee8d28d66930803
paul-beckett <beckettp@gmail.com>
2025-12-11 14:47:57 +0000

    aoc2025 - 2025 day11 part2

--- challenge/aoc2025/day11/day11.go
+++ challenge/aoc2025/day11/day11.go
@@ -1,7 +1,6 @@
 package day11
 
 import (
-	"errors"
 	"strings"
 	"unicode"
 )
@@ -23,26 +22,38 @@ func NewDay11(data []string) *Day11 {
 }
 
 func (d *Day11) Part1() (int, error) {
-	return d.countPaths("you", "out", make(map[string]bool)), nil
+	return d.countPaths("you", "out"), nil
 }
 
-func (d *Day11) countPaths(src string, target string, visited map[string]bool) int {
-
+func (d *Day11) dfs(src string, target string, memo map[string]int) int {
 	if src == target {
 		return 1
 	}
 
-	visited[src] = true
+	if val, ok := memo[src]; ok {
+		return val
+	}
+
 	paths := 0
 	for _, next := range d.graph[src] {
-		if !visited[next] {
-			paths += d.countPaths(next, target, visited)
-		}
+		paths += d.dfs(next, target, memo)
 	}
-	visited[src] = false
+	memo[src] = paths
 	return paths
 }
 
+func (d *Day11) countPaths(src string, target string) int {
+	return d.dfs(src, target, make(map[string]int))
+}
+
+func (d *Day11) countPathsVia(paths []string) int {
+	total := 1
+	for i, path := range paths[:len(paths)-1] {
+		total *= d.countPaths(path, paths[i+1])
+	}
+	return total
+}
+
 func (d *Day11) Part2() (int, error) {
-	return 0, errors.ErrUnsupported
+	return d.countPathsVia([]string{"svr", "dac", "fft", "out"}) + d.countPathsVia([]string{"svr", "fft", "dac", "out"}), nil
 }
